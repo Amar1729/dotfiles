7b92f0faf40fce2cbb78023d375439f22128780e
Amar1729 <amar.paul16@gmail.com>
2018-04-28 20:26:48 -0400

    unique_spaces caches more stuff

--- .config/scripts/unique_space.sh
+++ .config/scripts/unique_space.sh
@@ -23,12 +23,30 @@ _cache_seq () {
 	cp ~/.cache/wal/sequences ~/.cache/wal/sequences_"$SPACE"
 }
 
+_cache_css () {
+	SPACE=$(_space_id)
+
+	cp ~/.cache/wal/colors.css ~/.cache/wal/"colors_""$SPACE"".css"
+}
+
+_cache_json() {
+	SPACE=$(_space_id)
+
+	cp ~/.cache/wal/colors.json ~/.cache/wal/"colors_""$SPACE"".json"
+}
+
+_cache() {
+	_cache_seq
+	#_cache_css
+	_cache_json
+}
+
 # TODO - add a check for sequences files that apply to destroyed desktops?
 change_wallpaper () {
 	FILE="$(realpath "$1")"
 	if [[ ! -f "$1" ]]; then exit 1; fi
 
-	(_run_wal "$FILE" && _cache_seq) >/dev/null 2>&1 &
+	(_run_wal "$FILE" && _cache) >/dev/null 2>&1 &
 	osascript -e "tell application \"Finder\" to set desktop picture to POSIX file \"$FILE\""
 }
 
