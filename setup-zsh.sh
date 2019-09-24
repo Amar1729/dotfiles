615888d7389a911870d51244988ebc11086ff6c7
Amar1729 <amar.paul16@gmail.com>
2019-09-24 13:00:41 -0400

    fixes to zsh setup script

--- setup-zsh.sh
+++ setup-zsh.sh
@@ -4,27 +4,36 @@
 # curl -sfL git.io/antibody | sh -s - -b /usr/local/bin
 
 path_fix () {
-    if ! grep '\.local\/bin' "$PATH"; then
+    if ! grep '\.local\/bin' "$PATH" 2>/dev/null; then
         echo 'export $PATH="$PATH:$HOME/.local/bin' >> ~/.profile
     fi
     mkdir -p ~/.local/bin
 }
 
 main () {
+    set -e
+
     path_fix
-    curl -sfL git.io/antibody -o ~/.local/bin/antibody && chmod +x ~/.local/bin/antibody
+    curl -sfL git.io/antibody | sh -s -- -b ~/.local/bin
 
     curl -sfL https://raw.githubusercontent.com/Amar1729/dotfiles/simple/.zshrc -o ~/.zshrc
+    curl -sfL https://raw.githubusercontent.com/Amar1729/dotfiles/simple/.zsh_plugins.txt -o ~/.zsh_plugins.txt
 
     ~/.local/bin/antibody bundle < ~/.zsh_plugins.txt > ~/.zsh_plugins.zsh
 
-    printf "Installed antibody and zsh plugins. You can run:"
+    echo "Installed antibody and zsh plugins. You can run:"
     echo "$ chsh -s $(command -v zsh)"
-    printf "Make sure to add the shell to /etc/shells if necessary."
+    echo "Make sure to add the shell to /etc/shells if necessary."
 }
 
-if command -v zsh 2>/dev/null; then
-    main
-else
-    printf "Install zsh first please"
+if ! command -v git 2>/dev/null; then
+    echo "Install git first please"
+    exit 1
 fi
+
+if ! command -v zsh 2>/dev/null; then
+    echo "Install zsh first please"
+    exit 1
+fi
+
+main
