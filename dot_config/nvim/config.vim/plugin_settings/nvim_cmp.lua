e761ade43e3697ac1d900853d5a801e8defb814f
Amar1729 <amar.paul16@gmail.com>
2022-10-22 11:53:17 -0400

    +~ vim: update cmp_nvim_lsp capabilities, make diagnostics less noisy

--- dot_config/nvim/config.vim/plugin_settings/nvim_cmp.lua
+++ dot_config/nvim/config.vim/plugin_settings/nvim_cmp.lua
@@ -107,7 +107,7 @@ cmp.setup.cmdline(':', {
 })
 
 -- Setup lspconfig.
-local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
 local lspconfig = require('lspconfig')
 
 local opts = { noremap=true, silent=true }
@@ -115,6 +115,12 @@ vim.api.nvim_set_keymap('n', '<space>e', '<cmd>lua vim.diagnostic.open_float()<C
 vim.api.nvim_set_keymap('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<CR>', opts)
 vim.api.nvim_set_keymap('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<CR>', opts)
 vim.api.nvim_set_keymap('n', '<space>q', '<cmd>lua vim.diagnostic.setloclist()<CR>', opts)
+
+-- mute diagnostics by default, too noisy
+vim.diagnostic.config({
+    virtual_text = false,
+    severity_sort = true,
+})
 
 -- Use an on_attach function to only map the following keys
 -- after the language server attaches to the current buffer
