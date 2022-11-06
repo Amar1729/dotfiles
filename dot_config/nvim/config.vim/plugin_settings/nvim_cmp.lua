bddc8f7a15ff4ec52cc7a84854ade27034fa821c
Amar1729 <amar.paul16@gmail.com>
2022-11-05 22:18:50 -0400

    - vim: omnifunc map not needed as of nvim 0.8

--- dot_config/nvim/config.vim/plugin_settings/nvim_cmp.lua
+++ dot_config/nvim/config.vim/plugin_settings/nvim_cmp.lua
@@ -142,9 +142,6 @@ vim.diagnostic.config({
 -- Use an on_attach function to only map the following keys
 -- after the language server attaches to the current buffer
 local on_attach = function(client, bufnr)
-  -- Enable completion triggered by <c-x><c-o>
-  vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
-
   -- Mappings.
   -- See `:help vim.lsp.*` for documentation on any of the below functions
   local bufopts = { noremap=true, silent=true, buffer=bufnr }
