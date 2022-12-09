d78fdf392a0f43d4dd4e00922960b830d59c452d
Amar1729 <amar.paul16@gmail.com>
2022-12-09 14:35:24 -0500

    -+ nvim lua: migrate vanilla keymaps, LSP diagnostics. remove init.vim edit maps

--- dot_config/nvim/config.vim/plugin_settings/nvim_cmp.lua
+++ dot_config/nvim/config.vim/plugin_settings/nvim_cmp.lua
@@ -138,12 +138,6 @@ require('neodev').setup({
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
 local lspconfig = require('lspconfig')
 
-local opts = { noremap=true, silent=true }
-vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, opts)
-vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
-vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
-vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts)
-
 -- mute diagnostics by default, too noisy
 vim.diagnostic.config({
     virtual_text = false,
