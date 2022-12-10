c54fcea5314583f693e48e7c7a832c367c1eda62
Amar1729 <amar.paul16@gmail.com>
2022-12-09 20:24:49 -0500

    init.vim -> init.lua: complete vim->lua switchover!

---
+++ dot_config/nvim/init.lua
@@ -0,0 +1,10 @@
+-- Amar Paul's init.lua (for Neovim)
+
+-- install and set up plugins
+require "core.plugins"
+
+-- settings / bindings (note that some are dependent on loaded plugins, e.g. bindings/highlights)
+require "core.options"
+require "core.autocmds"
+require "core.mappings"
+require "core.highlights"
