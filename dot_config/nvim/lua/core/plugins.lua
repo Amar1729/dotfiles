ca5eb73ea77a61127b0bd6112552f197cdb30531
Amar1729 <amar.paul16@gmail.com>
2023-03-12 18:08:22 -0400

    + nvim plugins: add (git) diffview plugin

--- dot_config/nvim/lua/core/plugins.lua
+++ dot_config/nvim/lua/core/plugins.lua
@@ -276,6 +276,17 @@ return packer.startup(function(use)
         config = function() require "configs.gitsigns" end,
     }
 
+    -- better viewer for git history, git diffs
+    use {
+        "sindrets/diffview.nvim",
+        requires = "nvim-lua/plenary.nvim",
+        config = function ()
+            require("diffview").setup({
+                use_icons = false,
+            })
+        end
+    }
+
     -- use git from inside vim (e.g. `:Git commit`)
     use { "tpope/vim-fugitive" }
 
