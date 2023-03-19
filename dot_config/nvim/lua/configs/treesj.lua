52ea452b687e162928e216f3719d80ad92cdc3e4
Amar1729 <amar.paul16@gmail.com>
2023-03-19 01:55:20 -0400

    - nvim plugin: treesj remove some boilerplate after updating plugin

--- dot_config/nvim/lua/configs/treesj.lua
+++ dot_config/nvim/lua/configs/treesj.lua
@@ -10,32 +10,13 @@ local opts = {
     },
 }
 
-local clean_join = {
-    join = {
-        space_in_brackets = false,
-    },
-}
-
-local langs = {
-    -- current config for python - try to match what `black` would do
-    -- (e.g. trailing commas after last element in multiline obj)
-    -- TODO - is it possible to recursive split only when cursor is on an
-    -- object inside an object? eg [ [ x ] ] but not [ x [ ] ] ?
-    python = {
-        -- yes trailing comma on split
-        array = tsj_utils.set_preset_for_list(opts),
-        list = tsj_utils.set_preset_for_list(opts),
-        dictionary = tsj_utils.set_preset_for_dict(opts),
-        parameters = tsj_utils.set_preset_for_list(clean_join),
-        argument_list = tsj_utils.set_preset_for_list(opts),
-
-        -- no trailing comma on split
-        -- don"t surround with spaces on join
-        list_comprehension = tsj_utils.set_preset_for_args(clean_join),
-        dictionary_comprehension = tsj_utils.set_preset_for_args(clean_join),
-    },
-}
-
 tsj.setup({
-    langs = langs,
+    langs = {
+        python = {
+            -- override defaults: when splitting params/args, ensure last arg
+            -- has a trailing comma (cleaner diffs)
+            parameters = tsj_utils.set_preset_for_list(opts),
+            argument_list = tsj_utils.set_preset_for_list(opts),
+        },
+    },
 })
