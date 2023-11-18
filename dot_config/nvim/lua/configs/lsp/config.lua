d1e6796980ed171bbb30441a1634223573b6e094
Amar1729 <amar.paul16@gmail.com>
2023-11-18 01:40:11 -0500

    ~ nvim(lsp): rework LSP on_attach to autocmd

--- dot_config/nvim/lua/configs/lsp/config.lua
+++ dot_config/nvim/lua/configs/lsp/config.lua
@@ -1,34 +1,35 @@
 local M = {}
 
--- Use an on_attach function to only map the following keys
--- after the language server attaches to the current buffer
-function M.on_attach(client, bufnr)
-  -- Mappings.
-  -- See `:help vim.lsp.*` for documentation on any of the below functions
-  local bufopts = { noremap=true, silent=true, buffer=bufnr }
-  local buf_keymap = function(m, s, c) vim.keymap.set(m, s, c, bufopts) end
+-- only map the following keys after LSP attaches to the current buffer
+vim.api.nvim_create_autocmd("LspAttach", {
+  group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+  callback = function (ev)
+    vim.bo[ev.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
 
-  buf_keymap("n", "gD", vim.lsp.buf.declaration)
-  buf_keymap("n", "gd", vim.lsp.buf.definition)
-  buf_keymap("n", "K", vim.lsp.buf.hover)
-  buf_keymap("n", "gi", vim.lsp.buf.implementation)
-  buf_keymap("n", "<space>wa", vim.lsp.buf.add_workspace_folder)
-  buf_keymap("n", "<space>wr", vim.lsp.buf.remove_workspace_folder)
-  buf_keymap("n", "<space>wl", function()
-      print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-  end)
-  buf_keymap("n", "<space>D", vim.lsp.buf.type_definition)
-  buf_keymap("n", "<space>rn", vim.lsp.buf.rename)
-  buf_keymap("n", "<space>ca", vim.lsp.buf.code_action)
+    local opts = { noremap=true, silent=true, buffer=ev.buf }
+    vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+    vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+    vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+    vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+    vim.keymap.set("n", "<space>wa", vim.lsp.buf.add_workspace_folder, opts)
+    vim.keymap.set("n", "<space>wr", vim.lsp.buf.remove_workspace_folder, opts)
+    vim.keymap.set("n", "<space>wl", function()
+        print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+    end, opts)
+    vim.keymap.set("n", "<space>D", vim.lsp.buf.type_definition, opts)
+    vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, opts)
+    vim.keymap.set("n", "<space>ca", vim.lsp.buf.code_action, opts)
 
-  -- built-in LSP / telescope / glance ?
-  buf_keymap("n", "gr", vim.lsp.buf.references)
+    -- built-in LSP / telescope / glance ?
+    vim.keymap.set("n", "gr", vim.lsp.buf.references)
 
-  buf_keymap("n", "<space>f", function() vim.lsp.buf.format { async = true } end)
+    vim.keymap.set("n", "<space>f", function() vim.lsp.buf.format { async = true } end, opts)
 
-  -- for lvimuser/lsp-inlayhints.nvim
-  require("lsp-inlayhints").on_attach(client, bufnr)
-end
+    -- for lvimuser/lsp-inlayhints.nvim
+    local client = vim.lsp.get_client_by_id(ev.data.client_id)
+    require("lsp-inlayhints").on_attach(client, ev.buf)
+  end
+})
 
 -- restrain a language server (in the case that i"m using multiple for same language)
 function M.on_attach_restrained(client, buffer)
