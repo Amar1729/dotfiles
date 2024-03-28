23678bedaa284e264ec6386e2c2faedc64903a11
Amar1729 <amar.paul16@gmail.com>
2024-03-28 02:17:36 -0400

    +~ nvim(ts): fix python injections for SQL syntax highlighting

--- dot_config/nvim/after/queries/python/injections.scm
+++ dot_config/nvim/after/queries/python/injections.scm
@@ -1,10 +1,21 @@
 ;; extends
 
-; apply sql syntax highlight to any var whose name matches QUERY
+; apply sql syntax highlight to any var whose name matches QUERY_ or SQL_
 (assignment
-  left: (identifier) @_id (#match? @_id "QUERY")
-  right: (string string_content: (string_content)) @sql
-  (#offset! @sql 0 0 0 0))
+  left: (identifier) @_id (#match? @_id "QUERY_|SQL_")
+  right: (string (string_content) @injection.content)
+  (#set! injection.language "sql")
+  (#offset! @injection.content 0 0 0 0)
+  (#set! injection.include-children))
 
 ;; TODO?
 ; https://github.com/tree-sitter/tree-sitter/discussions/1577
+
+; capture that highlights the _first_ string inside any function named foo.execute() as SQL
+; (typically cur.execute())
+(call
+  function: (attribute attribute: (identifier) @id (#match? @id "execute"))
+  arguments: (argument_list . (string (string_content) @injection.content))
+  (#set! injection.language "sql")
+  (#offset! @injection.content 0 0 0 0)
+  (#set! injection.include-children))
